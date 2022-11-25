6ec83182f8585ad2ac3f213bfc39980fa8d23eb2
Hazem Mohamed <hazem.mohamed@cultiv.llc>
2022-11-25 13:09:23 +0200

    install: first commit

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Button = ({color, text, onClick }) => {
+  return <button onClick={onClick} style={{ backgroundColor: color}} className='btn'>{text}</button>
+}
+Button.defaultProps = {
+    text: 'button',
+    color: 'steelblue',
+}
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func
+}
+export default  Button 
